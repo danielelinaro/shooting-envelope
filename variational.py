2b6931c65d831371dd72339fd4be9d4b5d154aee
Daniele Linaro <danielelinaro@gmail.com>
2019-04-15 14:48:54 +0200

    Minor changes to variational.py.

--- variational.py
+++ variational.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-from envel import TrapEnvelope, BEEnvelope
-from systems import vdp, vdp_jac
+from polimi.envel import TrapEnvelope, BEEnvelope
+from polimi.systems import vdp, vdp_jac
 
 import ipdb
 
