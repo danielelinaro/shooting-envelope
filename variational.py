3f4e0fc1504c75101345062c426516bd55ff83ed
Daniele Linaro <danielelinaro@gmail.com>
2019-04-15 17:47:33 +0200

    Added Van der Pol example with one forcing frequency which is a multiple of 2*pi.

--- variational.py
+++ variational.py
@@ -90,6 +90,35 @@ def variational_vdp():
     plt.show()
 
     
+def variational_vdp_one_freq():
+    epsilon = 1e-3
+    A = [1]
+    T_small = 2*np.pi
+    T_large = 100*T_small
+    T = [T_large]
+    fun = lambda t,y: vdp(t,y,epsilon,A,T)
+    jac = lambda t,y: vdp_jac(t,y,epsilon)
+    var_fun = lambda t,y: variational_system(fun, jac, t, y, T_large)
+
+    t_span_var = [0,1]
+    y0 = np.array([-0.41338977, -0.00832443])
+    y0_var = np.concatenate((y0,np.eye(len(y0)).flatten()))
+
+    atol = [1e-2,1e-2,1e-6,1e-6,1e-6,1e-6]
+    var_sol = solve_ivp(var_fun, t_span_var, y0_var, rtol=1e-7, atol=1e-8,
+                        dense_output=True)
+    var_envelope_solver = TrapEnvelope(var_fun, t_span_var, y0_var, T_guess=None,
+                                       T=T_small/T_large, rtol=1e-1, atol=atol)
+    var_env = var_envelope_solver.solve()
+
+    plt.plot(t_span_var,[0,0],'b')
+    plt.plot(var_sol['t'],var_sol['y'][2],'k')
+    plt.plot(var_env['t'],var_env['y'][2],'mo')
+    plt.ion()
+    plt.show()
+
+
 if __name__ == '__main__':
+    #variational_linear()
     #variational_vdp()
-    variational_linear()
+    variational_vdp_one_freq()
