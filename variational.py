d804d779a869f00f00df5d628ca1d16bee4f5fd4
Daniele Linaro <danielelinaro@gmail.com>
2019-04-15 17:16:55 +0200

    Added forced linear system to variational.py.

--- variational.py
+++ variational.py
@@ -17,7 +17,53 @@ def variational_system(fun, jac, t, y, T):
                            T * np.matmul(J,phi).flatten()))
 
 
-def main():
+def linear(t,y,A,B,T):
+    u = lambda t: np.array([np.cos(2*np.pi*t/TT) for TT in T])
+    return np.matmul(A,y) + np.matmul(B,u(t))
+
+
+def variational_linear():
+    A = np.array([[0,1],[-1,-0.001]])
+    B = np.array([[0],[10]])
+    T_small = 2*np.pi
+    T_large = 100*T_small
+    T = [T_large]
+    fun = lambda t,y: linear(t,y,A,B,T)
+    jac = lambda t,y: A
+    var_fun = lambda t,y: variational_system(fun, jac, t, y, T_large)
+    
+    t_span = [0,T_large]
+    sol = solve_ivp(fun, t_span, [1,0], method='RK45', rtol=1e-8, atol=1e-10)
+    y0 = sol['y'][:,-1]
+    t_span_var = [0,1]
+    y0_var = np.concatenate((y0,np.eye(len(y0)).flatten()))
+    
+    sol = solve_ivp(fun, t_span, y0, method='RK45', rtol=1e-8, atol=1e-10)
+    envelope_solver = TrapEnvelope(fun, t_span, y0, T_guess=None,
+                                   T=T_small, rtol=1e-2, atol=1e-4)
+    env = envelope_solver.solve()
+
+    plt.plot(sol['t'],sol['y'][0],'k',label='y_1')
+    plt.plot(env['t'],env['y'][0],'ro',label='y_1 env')
+    plt.plot(sol['t'],sol['y'][1],'m',label='y_2')
+    plt.plot(env['t'],env['y'][1],'go',label='y_2 env')
+    plt.legend(loc='best')
+    plt.show()
+
+    atol = [1e-2,1e-2,1e-6,1e-6,1e-6,1e-6]
+    var_sol = solve_ivp(var_fun, t_span_var, y0_var, rtol=1e-7, atol=1e-8,
+                        dense_output=True)
+    var_envelope_solver = TrapEnvelope(var_fun, t_span_var, y0_var, T_guess=None,
+                                       T=T_small/T_large, rtol=1e-1, atol=atol)
+    var_env = var_envelope_solver.solve()
+
+    plt.plot(t_span_var,[0,0],'b')
+    plt.plot(var_sol['t'],var_sol['y'][3],'k')
+    plt.plot(var_env['t'],var_env['y'][3],'mo')
+    plt.show()
+
+
+def variational_vdp():
     epsilon = 1e-3
     A = [10,1]
     T = [4,400]
@@ -32,13 +78,10 @@ def main():
     y0_var = np.concatenate((y0,np.eye(len(y0)).flatten()))
 
     atol = [1e-2,1e-2,1e-6,1e-6,1e-6,1e-6]
-
     var_sol = solve_ivp(var_fun, t_span_var, y0_var, rtol=1e-7, atol=1e-8,
                         dense_output=True)
     var_envelope_solver = TrapEnvelope(var_fun, t_span_var, y0_var, T_guess=None,
                                        T=T_small/T_large, rtol=1e-1, atol=atol)
-    #var_envelope_solver = BEEnvelope(var_fun, t_span_var, y0_var, T_guess=None,
-    #                                 T=T_small/T_large, rtol=1e-1, atol=atol)
     var_env = var_envelope_solver.solve()
 
     plt.plot(t_span_var,[0,0],'b')
@@ -48,4 +91,5 @@ def main():
 
     
 if __name__ == '__main__':
-    main()
+    #variational_vdp()
+    variational_linear()
