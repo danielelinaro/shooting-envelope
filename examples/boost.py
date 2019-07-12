a2bbc258a6e1079f4d4756948c2fcac6d481d3f3
Daniele Linaro <danielelinaro@gmail.com>
2019-07-12 09:21:59 +0200

    Modified boost example.

--- examples/boost.py
+++ examples/boost.py
@@ -188,58 +188,75 @@ def variational_integration():
     plt.show()
 
 
-def variational():
-
-    def variational_system(fun, jac, t, y, T):
-        N = int((-1 + np.sqrt(1 + 4*len(y))) / 2)
-        J = jac(t*T,y[:N])
-        phi = np.reshape(y[N:N+N**2],(N,N))
-        return np.concatenate((T * fun(t*T, y[:N]), \
-                               T * (J @ phi).flatten()))
-
+def variational_envelope():
     T = 20e-6
-    DC = 0.45
-    T_large = 200*T
-    T_small = T
-    fun = lambda t,y: boost(t,y,T,DC)
-    jac = lambda t,y: boost_jac(t,y,T,DC)
-    var_fun = lambda t,y: variational_system(fun, jac, t, y, T_large)
+    ki = 1
+    Vin = 5
+    Vref = 5
+
+    boost = Boost(0, T=T, ki=ki, Vin=Vin, Vref=Vref, clock_phase=0)
+
     fun_rtol = 1e-10
     fun_atol = 1e-12
 
+    y0 = np.array([Vin,1])
+    t_tran = 50*T
+
+    print('Vector field index at the beginning of the first integration: %d.' % boost.vector_field_index)
+    sol = solve_ivp_switch(boost, [0,t_tran], y0, \
+                           method='BDF', jac=boost.J, \
+                           rtol=fun_rtol, atol=fun_atol)
+    print('Vector field index at the end of the first integration: %d.' % boost.vector_field_index)
+    #plt.figure()
+    #ax = plt.subplot(2,1,1)
+    #plt.plot(sol['t']*1e6,sol['y'][0],'k')
+    #plt.ylabel(r'$V_C$ (V)')
+    #plt.subplot(2,1,2,sharex=ax)
+    #plt.plot(sol['t']*1e6,sol['y'][1],'r')
+    #plt.xlabel(r'Time ($\mu$s)')
+    #plt.ylabel(r'$I_L$ (A)')
+    #plt.show()
+
+    y0 = sol['y'][:,-1]
+
+    T_large = 100*T
+    T_small = T
+    boost.with_variational = True
+    boost.variational_T = T_large
+
     t_span_var = [0,1]
-    y0 = np.array([9.17375836, 1.00930474])
     y0_var = np.concatenate((y0,np.eye(len(y0)).flatten()))
+
+    sol = solve_ivp_switch(boost, t_span_var, y0_var, method='BDF', rtol=fun_rtol, atol=fun_atol)
 
     rtol = 1e-1
     atol = 1e-2
-    be_var_solver = BEEnvelope(fun, [0,T_large], y0, T_guess=None, T=T_small, jac=jac, \
-                               rtol=rtol, atol=atol, fun_method='BDF', max_step=1000, \
-                               fun_rtol=fun_rtol, fun_atol=fun_atol, is_variational=True, \
-                               T_var_guess=None, T_var=None, var_rtol=rtol, var_atol=atol)
-    trap_var_solver = TrapEnvelope(fun, [0,T_large], y0, T_guess=None, T=T_small, jac=jac, \
-                                   rtol=rtol, atol=atol, fun_method='BDF', max_step=100, \
-                                   fun_rtol=fun_rtol, fun_atol=fun_atol, is_variational=True, \
-                                   T_var_guess=None, T_var=None, var_rtol=rtol, var_atol=atol)
+    be_var_solver = BEEnvelope(boost, [0,T_large], y0, T_guess=None, T=T_small, jac=boost.J, \
+                               rtol=rtol, atol=atol, fun_method=solve_ivp_switch, \
+                               max_step=1000, fun_rtol=fun_rtol, fun_atol=fun_atol, \
+                               is_variational=True, T_var_guess=None, T_var=None, \
+                               var_rtol=rtol, var_atol=atol, method='BDF')
+    #trap_var_solver = TrapEnvelope(fun, [0,T_large], y0, T_guess=None, T=T_small, jac=jac, \
+    #                               rtol=rtol, atol=atol, fun_method='BDF', max_step=100, \
+    #                               fun_rtol=fun_rtol, fun_atol=fun_atol, is_variational=True, \
+    #                               T_var_guess=None, T_var=None, var_rtol=rtol, var_atol=atol)
     print('-' * 100)
     var_sol_be = be_var_solver.solve()
     print('-' * 100)
-    var_sol_trap = trap_var_solver.solve()
-    print('-' * 100)
-
-    sol = solve_ivp(var_fun, t_span_var, y0_var, method='BDF', rtol=fun_rtol, atol=fun_atol)
+    #var_sol_trap = trap_var_solver.solve()
+    #print('-' * 100)
 
     eig,_ = np.linalg.eig(np.reshape(sol['y'][2:,-1],(2,2)))
     print('         correct eigenvalues:', eig)
     eig,_ = np.linalg.eig(np.reshape(var_sol_be['y'][2:,-1],(2,2)))
     print('  BE approximate eigenvalues:', eig)
-    eig,_ = np.linalg.eig(np.reshape(var_sol_trap['y'][2:,-1],(2,2)))
-    print('TRAP approximate eigenvalues:', eig)
+    #eig,_ = np.linalg.eig(np.reshape(var_sol_trap['y'][2:,-1],(2,2)))
+    #print('TRAP approximate eigenvalues:', eig)
 
     ax = plt.subplot(1,2,1)
     plt.plot(sol['t'],sol['y'][1],'k')
     plt.plot(var_sol_be['t'],var_sol_be['y'][1],'ro-')
-    plt.plot(var_sol_trap['t'],var_sol_trap['y'][1],'go')
+    #plt.plot(var_sol_trap['t'],var_sol_trap['y'][1],'go')
     plt.xlabel('Normalized time')
     plt.ylabel(r'$I_L$ (A)')
     plt.subplot(1,2,2,sharex=ax)
@@ -248,7 +265,7 @@ def variational():
     plt.plot(var_sol_be['t'],var_sol_be['y'][2],'ro')
     #for i in range(0,len(var_sol_be['var']['t']),3):
     #    plt.plot(var_sol_be['var']['t'][i:i+3],var_sol_be['var']['y'][0,i:i+3],'c.-')
-    plt.plot(var_sol_trap['t'],var_sol_trap['y'][2],'go')
+    #plt.plot(var_sol_trap['t'],var_sol_trap['y'][2],'go')
     #for i in range(0,len(var_sol_trap['var']['t']),3):
     #    plt.plot(var_sol_trap['var']['t'][i:i+3],var_sol_trap['var']['y'][0,i:i+3],'m.-')
     plt.xlabel('Normalized time')
@@ -260,4 +277,5 @@ if __name__ == '__main__':
     #system()
     #envelope()
     variational_integration()
-    #variational()
+    # the following example does not work
+    #variational_envelope()
