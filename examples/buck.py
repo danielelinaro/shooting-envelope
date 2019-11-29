Fixed a couple of bugs in envelope computation

examples/buck.py
@@ -61,7 +61,7 @@ def dump_data(outfile, **kwargs):
 
 
 def make_full_envelope_solution(ckt, sol_env, T):
-    for t0,y0 in zip(sol_env['t'],sol_env['y'].T):
+    for t0,y0 in zip(sol_env['t'][:-1],sol_env['y'][:,:-1].T):
         if ckt.with_variational:
             sol = solve_ivp_switch(ckt, [t0,t0+T], y0, method='BDF', \
                                    rtol=fun['rtol'], atol=fun['atol'])
@@ -73,6 +73,8 @@ def make_full_envelope_solution(ckt, sol_env, T):
             envelope['y'] = np.append(envelope['y'], sol['y'], axis=1)
         except:
             envelope = {key: sol[key] for key in ('t','y')}
+    envelope['t'] = np.append(envelope['t'], sol_env['t'][-1])
+    envelope['y'] = np.append(envelope['y'], np.reshape(sol_env['y'][:,-1], (3,1)), axis=1)
     return envelope
 
 

polimi/envelope/core.py
@@ -268,8 +268,8 @@ class EnvelopeSolver (object):
         y_cur = self.y[:,-1]
         f_cur = self.f[:,-1]
 
-        if t_cur + H > self.t_span[1]:
-            H = self.T * np.max((1,np.floor((self.t_span[1] - t_cur) / self.T)))
+        if t_cur + H > self.t_span[1] + self.T:
+            H = self.T * np.max((1,np.floor((self.t_span[1] + self.T - t_cur) / self.T)))
         t_next = t_cur + H
 
         # step size in units of period
@@ -323,8 +323,8 @@ class EnvelopeSolver (object):
 
         # compute the new value of H as the maximum value that allows having an LTE below threshold
         self.H_new = self._compute_next_H(scale, coeff, T)
-        if t_next + self.H_new > self.t_span[1]:
-            self.H_new = self.T * np.floor((self.t_span[1] - t_next) / self.T)
+        if t_next + self.H_new > self.t_span[1] + self.T:
+            self.H_new = self.T * np.floor((self.t_span[1] + self.T - t_next) / self.T)
 
         if self.is_variational and self.compute_variational_LTE and n_periods_var > 1:
             self.H_new = np.min((self.H_new, np.floor(H_new_var / T) * T))
@@ -415,9 +415,10 @@ class EnvelopeSolver (object):
 
         if T_guess is None:
             T_guess = self.T
+
         # find the equation of the plane containing y and
         # orthogonal to fun(t,y)
-        f = self.system(t,y)
+        f = self.system(t,y_ext)
         w = f[self.vars_to_use] / np.linalg.norm(f[self.vars_to_use])
         b = -np.dot(w, y[self.vars_to_use])
 
@@ -437,8 +438,6 @@ class EnvelopeSolver (object):
             T = sol['t_events'][0][idx[0]] - t
         except:
             T = T_guess
-            if self.verbose:
-                print('EnvelopeSolver._envelope_fun(%.4e)> T = T_guess = %.3e.' % (t,T))
 
         self.T_new = T
         self.t_new = t + self.T_new
@@ -446,7 +445,7 @@ class EnvelopeSolver (object):
         self.y_new = Y[:n_dim]
         self._last_envelope_fun_call['phi'] = np.reshape(Y[n_dim:],(n_dim,n_dim)).copy()
         # return the "vector field" of the envelope
-        return 1./self.T_new * (self.y_new - sol['sol'](t))
+        return 1./self.T_new * (self.y_new - y)
 
 
     def _get_stored_monodromy_matrix(self, t, y):

tests/envelope_tests.py
@@ -126,53 +126,59 @@ def HR():
     y0 = [0,1,0.1]
     #y0 = np.array([-0.85477615, -3.03356705,  4.73029393])
     t_tran = 100
-    sol = solve_ivp(hr, [0,t_tran], y0, method='RK45', rtol=1e-8, atol=1e-10)
+    rtol = 1e-6
+    atol = 1e-8
+    sol = solve_ivp(hr, [0,t_tran], y0, method='BDF', jac=hr.jac, rtol=rtol, atol=atol)
     y0 = sol['y'][:,-1]
 
-    t_span = [0,5000]
+    t_span = [0,500]
     T_guess = 11
 
     be_solver = BEEnvelope(hr, t_span, y0, T_guess=T_guess, \
-                           max_step=500, integer_steps=False, \
-                           env_rtol=1e-3, env_atol=1e-6, \
-                           rtol=1e-8, atol=1e-10)
+                           max_step=500, integer_steps=True, \
+                           env_rtol=1e-3, env_atol=1e-4, \
+                           solver=solve_ivp, method='BDF', jac=hr.jac, \
+                           rtol=rtol, atol=atol)
     trap_solver = TrapEnvelope(hr, t_span, y0, T_guess=T_guess, \
-                               max_steps=500, integer_steps=True, \
-                               env_rtol=1e-3, env_atol=1e-6, \
-                               rtol=1e-8, atol=1e-10)
+                               max_step=500, integer_steps=True, \
+                               env_rtol=1e-3, env_atol=1e-4, \
+                               solver=solve_ivp, method='BDF', jac=hr.jac, \
+                               rtol=rtol, atol=atol)
+    trap_solver.verbose = True
 
-    print('-' * 81)
-    sol_be = be_solver.solve()
+    #print('-' * 81)
+    #sol_be = be_solver.solve()
     print('-' * 81)
     sol_trap = trap_solver.solve()
     print('-' * 81)
-    sol = solve_ivp(hr, t_span, y0, method='RK45', rtol=1e-8, atol=1e-10)
+    sol = solve_ivp(hr, [t_span[0], sol_trap['t'][-1]], y0, method='BDF', \
+                    jac=hr.jac, rtol=rtol, atol=atol)
 
     fig,(ax1,ax2) = plt.subplots(1, 2, figsize=(10,6))
     ax1.plot(sol['t'],sol['y'][0],'k')
-    for t0,y0,T in zip(sol_be['t'],sol_be['y'].T,sol_be['T']):
-        period = solve_ivp(hr, [t0,t0+T], y0, method='RK45', rtol=1e-8, atol=1e-10)
-        ax1.plot(period['t'], period['y'][0], color=[1,.6,.6], lw=1)
+    #for t0,y0,T in zip(sol_be['t'],sol_be['y'].T,sol_be['T']):
+    #    period = solve_ivp(hr, [t0,t0+T], y0, method='RK45', rtol=rtol, atol=atol)
+    #    ax1.plot(period['t'], period['y'][0], color=[1,.6,.6], lw=1)
     for t0,y0,T in zip(sol_trap['t'],sol_trap['y'].T,sol_trap['T']):
-        period = solve_ivp(hr, [t0,t0+T], y0, method='RK45', rtol=1e-8, atol=1e-10)
+        period = solve_ivp(hr, [t0,t0+T], y0, method='BDF', rtol=rtol, atol=atol)
         ax1.plot(period['t'], period['y'][0], color=[.6,1,.6], lw=1)
-    ax1.plot(sol_be['t'],sol_be['y'][0],'ro-')
+    #ax1.plot(sol_be['t'],sol_be['y'][0],'ro-')
     ax1.plot(sol_trap['t'],sol_trap['y'][0],'go-')
-    ax1.plot(sol_be['t'],sol_be['T'],'ms-')
+    ax1.plot(sol_trap['t'],sol_trap['T'],'mo-')
     ax1.set_xlabel('t')
     ax1.set_ylabel('y')
 
-    idx, = np.where(sol['t'] > 1000)
-    ax2.plot(sol['y'][0,idx],sol['y'][1,idx],'k')
-    for t0,y0,T in zip(sol_be['t'],sol_be['y'].T,sol_be['T']):
-        if t0 < 1000:
-            continue
-        period = solve_ivp(hr, [t0,t0+T], y0, method='RK45', rtol=1e-8, atol=1e-10)
-        ax2.plot(period['y'][0], period['y'][1], color=[1,.6,.6], lw=1)
-    idx, = np.where(sol_be['t'] > 1000)
-    ax2.plot(sol_be['y'][0,idx],sol_be['y'][1,idx],'ro-')
-    ax2.set_xlabel('x')
-    ax2.set_ylabel('y')
+    #idx, = np.where(sol['t'] > 1000)
+    #ax2.plot(sol['y'][0,idx],sol_trap['y'][1,idx],'k')
+    #for t0,y0,T in zip(sol_trap['t'],sol_be['y'].T,sol_be['T']):
+    #    if t0 < 1000:
+    #        continue
+    #    period = solve_ivp(hr, [t0,t0+T], y0, method='RK45', rtol=1e-8, atol=1e-10)
+    #    ax2.plot(period['y'][0], period['y'][1], color=[1,.6,.6], lw=1)
+    #idx, = np.where(sol_be['t'] > 1000)
+    #ax2.plot(sol_trap['y'][0,idx],sol_trap['y'][1,idx],'ro-')
+    #ax2.set_xlabel('x')
+    #ax2.set_ylabel('y')
 
     plt.show()
 
@@ -181,7 +187,7 @@ def main():
     import polimi.utils
     polimi.utils.set_rc_defaults()
 
-    autonomous()
+    #autonomous()
     HR()
 
 